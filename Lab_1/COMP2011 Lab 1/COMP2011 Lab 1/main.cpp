b332d8d2c7a538e61cc9fef6262687d8aa7b74cb
Elliot Ng <chihimng.io@gmail.com>
2017-09-11 14:35:50 +0800

    use const for dict

--- Lab_1/COMP2011 Lab 1/COMP2011 Lab 1/main.cpp
+++ Lab_1/COMP2011 Lab 1/COMP2011 Lab 1/main.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     int month;
-    string monthDict[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-    string adjectives[] = {"intelligent", "beautiful", "sweet", "adventurous", "passionate", "reliable", "humble", "ecletic", "brave", "smart", "easygoing", "helpful"
+    const string MONTH_DICT[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    const string ADJECTIVES[] = {"intelligent", "beautiful", "sweet", "adventurous", "passionate", "reliable", "humble", "ecletic", "brave", "smart", "easygoing", "helpful"
     };
     bool isFinished = false;
     while (!isFinished) {
         cout << "Which month were you born in?\n";
         cin >> month;
         if (month >= 1 && month <= 12) {
-            cout << "I see! You were born in " + monthDict[month - 1] + ". You must be " + adjectives[month - 1] + "!\n";
+            cout << "I see! You were born in " + MONTH_DICT[month - 1] + ". You must be " + ADJECTIVES[month - 1] + "!\n";
         } else {
             cout << "Oh. You must be... crazy! Bye!\n";
             isFinished = true;
