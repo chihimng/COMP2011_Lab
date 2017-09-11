bbd73de564ed3a64b848ffc7d22ce9114d1c11ab
Elliot Ng <chihimng.io@gmail.com>
2017-09-11 12:47:53 +0800

    change exit flag name, use if-else instead of switch-case

--- Lab_1/COMP2011 Lab 1/COMP2011 Lab 1/main.cpp
+++ Lab_1/COMP2011 Lab 1/COMP2011 Lab 1/main.cpp
@@ -14,16 +14,15 @@ int main(int argc, const char * argv[]) {
     string monthDict[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     string adjectives[] = {"intelligent", "beautiful", "sweet", "adventurous", "passionate", "reliable", "humble", "ecletic", "brave", "smart", "easygoing", "helpful"
     };
-    bool finish = false;
-    while (!finish) {
+    bool isFinished = false;
+    while (!isFinished) {
         cout << "Which month were you born in?\n";
         cin >> month;
-        switch (month) {
-            case 1 ... 12 : cout << "I see! You were born in " + monthDict[month - 1] + ". You must be " + adjectives[month - 1] + "!\n";
-                break;
-            default: cout << "Oh. You must be... crazy! Bye!\n";
-                finish = true;
-                break;
+        if (month >= 1 && month <= 12) {
+            cout << "I see! You were born in " + monthDict[month - 1] + ". You must be " + adjectives[month - 1] + "!\n";
+        } else {
+            cout << "Oh. You must be... crazy! Bye!\n";
+            isFinished = true;
         }
     }
     return 0;
