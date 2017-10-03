cd2a2ebae49e24ec0d774e34c6d947e37396f0a2
Elliot Ng <chihimng.io@gmail.com>
2017-10-04 05:23:53 +0800

    implement lab 4

--- COMP2011 Lab 4/COMP2011 Lab 4/main.cpp
+++ COMP2011 Lab 4/COMP2011 Lab 4/main.cpp
@@ -10,10 +10,50 @@
 using namespace std;
 
 //Task 1 Write a function print_maze to print the maze.
+void print_maze(char maze[], int height, int width) {
+    for (int i = 0; i < height * width; i++) {
+        if (!(i % width)) cout << '\n' ;
+        cout << maze[i];
+    }
+    cout << '\n';
+}
 
 //Task 2 Write a function is_valid_move to check if the move is valid or not.
 
+bool is_valid_move(char maze[], int height, int width, int x, int y, char move) {
+    // Parse into 2D array
+    char maze2d[100][100] = {};
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            maze2d[i][j] = maze[width * i + j];
+        }
+    }
+
+    switch (move) {
+        case 'w':
+            if ((y - 1) < 0) return false;
+            return maze2d[y - 1][x] != '#';
+        case 'a':
+            if ((x - 1) < 0) return false;
+            return maze2d[y][x - 1] != '#';
+        case 's':
+            if ((y + 1) >= height) return false;
+            return maze2d[y + 1][x] != '#';
+        case 'd':
+            if ((x + 1) >= width) return false;
+            return maze2d[y][x + 1] != '#';
+        default:
+            return false;
+    }
+}
+
 //Task 3 Write a function update_maze to update the position of the player in the maze.
+void update_maze(char maze[], int height, int width, int x, int y) {
+    for (int i = 0; i < height * width; i++) {
+        if (maze[i] == 'P') maze[i] = ' ';
+    }
+    maze[y * width + x] = 'P';
+}
 
 int main() {
     const int HEIGHT = 9;
