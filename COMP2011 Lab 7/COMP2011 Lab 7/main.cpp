eca5b21d02111e2bd60616348264deadbd9aac5f
Elliot Ng <chihimng.io@gmail.com>
2017-11-01 15:37:25 +0800

    lab 7 init commit

---
+++ COMP2011 Lab 7/COMP2011 Lab 7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP2011 Lab 7
+//
+//  Created by Chihim Ng on 1/11/2017.
+//  Copyright © 2017 Chihim Ng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
